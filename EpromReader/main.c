2a61492f24486bfb3723d4a00f50f5efeeead1b0
Jon D <dyercode@users.noreply.github.com>
2020-04-21 22:40:43 -0400

    implement address + chip select loop.
    remove delays, they aren't needed now that it's pulsing correctly. left
    delay 0's in for now so I can quickly add them back if needed.
    switch up pins to atmega168a.
    Using portc to leave serial and ISP pins open. arduino uses this
    exclusively for analog, see if I need to do the same and use a different
    port.
    Fix a bunch of overflows from signed ints
    remove bitbang serial setup.

--- EpromReader/main.c
+++ EpromReader/main.c
@@ -2,39 +2,42 @@
  * EpromReader.c
  */
 
-#define FOSC 1843200 // Clock Speed
-//#define F_CPU 8000000UL
+//#define FOSC 1843200 // Clock Speed, but why this speed?
+#define F_CPU 8000000UL
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-#define TX_PIN PB4 // Pin 3
-#define TX_PIN_SET DDB4
-#define OUT_DATA PB0 // Pin 5
-#define OUT_DATA_SET DDB0
-#define OUT_LATCH PB1      // Pin 6
-#define OUT_LATCH_SET DDB1 // Pin 6
-#define OUT_CLOCK PB2      // Pin 7
-#define OUT_CLOCK_SET DDB2 // Pin 7
-#define MANUAL_TICK PB3    // Pin 2
+#define MYUBRR F_CPU / 16 / BAUD - 1
+//#define TX_PIN PB4 // Pin 3
+//#define TX_PIN_SET DDB4
+#define OUT_DATA PC0 // Pin 23
+#define OUT_DATA_SET DDC0
+#define OUT_LATCH PC1                 // Pin 24
+#define OUT_LATCH_SET DDC1            // Pin 24
+#define OUT_CLOCK PC2                 // Pin 25
+#define OUT_CLOCK_SET DDC2            // Pin 25
+#define MANUAL_TICK PC3               // Pin 26
+#define MANUAL_TICK_INTERRUPT PCINT11 // Pin 26
 #define ADDRESS_BASE 0x8000
 #define EPROM_SIZE 0x1FFF
-#define DIGITAL_DELAY 20
-#define ADDRESS_BIT_COUNT 8
+#define DIGITAL_DELAY_MS 0
+#define ADDRESS_BIT_COUNT 8 // mostly for testing since I'm short a shift register.
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
 //#include <util/setbaud.h>
 
-// basically kill me
-volatile uint16_t tx_shift_reg = 0;
+// delete meeee
 volatile uint16_t addr_tmp = 0;
 
 // void uart_init(void)
 void set_pin(uint8_t pin, uint8_t state);
 void uart_tx(char character);
 void uart_tx_string(char *string);
-void blit(int8_t address, int8_t chip_select);
-
+void blit(uint16_t address, uint8_t chip_select);
+void iterate_cart();
+void characterize(uint16_t d);
+void pulse();
+/*
 ISR(TIM0_COMPA_vect) {
   // set_pin(TX_PIN,1);
   // set_pin(TX_PIN,0);
@@ -53,23 +56,23 @@ ISR(TIM0_COMPA_vect) {
     TCNT0 = 0;
   }
 }
+*/
 
-ISR(PCINT0_vect) {
-  if (PINB & (1 << MANUAL_TICK)) {
+ISR(PCINT1_vect) {
+  if (PINC & (1 << MANUAL_TICK)) {
     addr_tmp += 1;
     blit(addr_tmp, 0b1111);
   }
 }
 
-void usart_init( unsigned int ubrr)
-{
-/*Set baud rate */
-UBRR0H = (unsigned char)(ubrr>>8);
-UBRR0L = (unsigned char)ubrr;
-Enable receiver and transmitter */
-UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-/* Set frame format: 8data, 2stop bit */
-UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+void usart_init(unsigned int ubrr) {
+  /*Set baud rate */
+  UBRR0H = (unsigned char)(ubrr >> 8);
+  UBRR0L = (unsigned char)ubrr;
+  /* Enable receiver and transmitter */
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+  /* Set frame format: 8data, 2stop bit */
+  UCSR0C = (0 << USBS0) | (3 << UCSZ00);
 }
 
 int main(void) {
@@ -77,7 +80,9 @@ int main(void) {
   CLKPR = 0x80; // set system clock to 8mhz with no prescale
   CLKPR = 0x00; // these 2 CLKPR instructions have to be run together in order
 
-  DDRB |= (1 << TX_PIN_SET | 1 << OUT_DATA_SET | 1 << OUT_LATCH_SET |
+  usart_init(MYUBRR);
+
+  DDRC |= (1 << OUT_DATA_SET | 1 << OUT_LATCH_SET |
            1 << OUT_CLOCK_SET); // mark output pins as such
 
   // CLKPCE = 1; // look this up
@@ -86,28 +91,28 @@ int main(void) {
   // CLKPS2 = 0;
   // CLKPS3 = 0;
 
-  usart_init(MYUBRR);
   // no prescaling cs02: 0, cs01: 0, cs00: 0
   // CTC mode 2, wgm02: 0, wgm01: 1, wgm00: 0
-  TCCR0A = (1 << WGM01);
-  TCCR0B = 0b00000000;
+  // TCCR0A = (1 << WGM01);
+  // TCCR0B = 0b00000000;
 
   // 833 = 8,000,000 / baudrate. 8mill = clock speed?
   //	OCR0A = 833 >> 8;  // first 3 binary digs
   // OCR0B = 833 & 0xFF; // last byte.
-  OCR0A = 103; // adjust somewhere around 103
+  // OCR0A = 103; // adjust somewhere around 103
 
-  TIMSK |= (1 << OCIE0A);
+  //  TIMSK |= (1 << OCIE0A);
 
-  GIMSK |= (1 << PCIE);   // pin change interrupt enable
-  PCMSK |= (1 << PCINT3); // pin change interrupt for PB3
+  // GIMSK |= (1 << PCIE);   // pin change interrupt enable
+  PCICR |= (1 << PCIE1);   // pin change interrupt enable
+  PCMSK1 |= (1 << PCINT3); // pin change interrupt for PB3
 
   sei();
 
   blit(0, 0b1111); // reset the shift registers
-  uart_tx_string("initialized...");
+  uart_tx_string("initialized...\r\n");
 
-  /* Replace with your application code */
+  iterate_cart(); // TODO - trigger by a serial read so it can started by pc and automated instead of just happeneing when it gets power.
   while (1) {
 
     // Cart slot has 15 address pins and 4 chip select pins. so going to need 3
@@ -116,17 +121,19 @@ int main(void) {
     // uart_tx_string("hello");
     //_delay_ms(100);
 
-    // iterate_cart(); this needs to be triggered by something so it only happens
-    // once. or just drop it out of the while loop.
+    // iterate_cart(); this needs to be triggered by something so it only
+    // happens once. or just drop it out of the while loop.
   }
 }
 
-uint8_t piso() {
+char read_eprom_byte() {
   /*
   PL (Parallel load) has to be tied high. pulse it low to latch the inputs.
   */
-  return 0;
+  return 'F';
 }
+
+uint8_t chip_bin(uint8_t n) { return 0b1111 & ~(1 << (4 - n)); }
 
 void iterate_cart() {
   /*
@@ -148,14 +155,31 @@ void iterate_cart() {
           7 spio clock
   */
 
-/*
-  int16_t range = 0x1fff
-  blit(addr, chip_select)
-  latch_piso()
-  let byte = read_data()
-  let strByte = convert byte to string
-  uart_tx_string(strByte)
-*/
+  /*
+    int16_t range = 0x1fff
+    blit(addr, chip_select)
+    latch_piso()
+    let byte = read_data()
+    let strByte = convert byte to string
+    uart_tx_string(strByte)
+  */
+  const uint16_t base = 0x8000;
+  const uint16_t range = 0x1fff;
+
+  for (uint8_t cs = 1; cs < 5; cs++) {
+    uint8_t chip_select = chip_bin(cs);
+    uint16_t offset = 0x2000 * (cs - 1);
+    uint16_t top = base + offset + range;
+    for (uint16_t addr = base + offset; addr <= top; addr++) {
+      blit(addr, chip_select);
+      _delay_us(0);
+      // latch_piso();
+      // char byte = read_eprom_byte();
+      // uart_tx(byte);
+      characterize(addr);
+    }
+  }
+  // uart_tx(EOF); not sure if will is work are
 
   /*
    design shift register output
@@ -188,16 +212,13 @@ void iterate_cart() {
   */
 }
 
-void shift_out(int8_t bit) {
+void shift_out(uint8_t bit) {
   set_pin(OUT_DATA, 1 & bit);
 
-  set_pin(OUT_CLOCK, 1);
-  _delay_ms(DIGITAL_DELAY);
-  set_pin(OUT_CLOCK, 0);
-  _delay_ms(DIGITAL_DELAY);
+  pulse_high(OUT_CLOCK);
 }
 
-void shift_many(int16_t data, int8_t size) {
+void shift_many(uint16_t data, uint8_t size) {
   int16_t local = data;
   for (int8_t i = 0; i < size; i++) {
     shift_out(local & 0b1);
@@ -205,53 +226,61 @@ void shift_many(int16_t data, int8_t size) {
   }
 }
 
-void blit(int16_t address, int8_t chip_select) {
+void characterize(uint16_t d) {
+  const unsigned char hexies[] = "0123456789ABCDEF";
+  unsigned char first_byte = ((d >> 12) & 0b1111);
+  unsigned char second_byte = ((d >> 8) & 0b1111);
+  unsigned char third_byte = ((d >> 4) & 0b1111);
+  unsigned char fourth_byte = (d & 0b1111);
+  uart_tx(hexies[first_byte]);
+  uart_tx(hexies[second_byte]);
+  uart_tx(hexies[third_byte]);
+  uart_tx(hexies[fourth_byte]);
+  uart_tx(' ');
+}
+
+void blit(uint16_t address, uint8_t chip_select) {
   // shift in chip_select first
   shift_many(chip_select, 4);
 
   // shift all the bits
   shift_many(address, ADDRESS_BIT_COUNT);
 
-  // pulse latch
-  set_pin(OUT_LATCH, 1);
-  _delay_ms(DIGITAL_DELAY);
-  set_pin(OUT_LATCH, 0);
-  _delay_ms(DIGITAL_DELAY);
+  pulse_high(OUT_LATCH);
 }
 
 void uart_tx(char character) {
   /* Wait for empty transmit buffer */
-  while ( !( UCSR0A & (1<<UDRE0)) );
+  while (!(UCSR0A & (1 << UDRE0)))
+    ;
   /* Put data into buffer, sends the data */
-  UDR0 = data;
+  UDR0 = character;
 }
 
 void uart_tx_string(char *string) {
   while (*string) {
     uart_tx(*string++);
     // wait until transmission is finished
-    while (tx_shift_reg)
-      ;
+    // while (!(UCSR0A & (1 << UDRE0)))
+    //;
   }
 }
 
 void set_pin(uint8_t pin, uint8_t state) {
   if (state) {
-    PORTB |= (1 << pin);
+    PORTC |= (1 << pin);
   } else {
-    PORTB &= ~(1 << pin);
+    PORTC &= ~(1 << pin);
   }
 }
 
-/*
-void uart_init(void) {
-//UBRR0H = UBRRH_VALUE;
-//UBRR0L = UBRRL_VALUE;
-//
-//#if USE_2X
-//UCSR0A |= _BV(U2X0);
-//#else
-//UCSR0A &= ~(_BV(U2X0));
-//#endif
+void pulse_high(uint8_t pin) { pulse(pin, true); }
+
+void pulse_low(uint8_t pin) { pulse(pin, false); }
+
+void pulse(uint8_t pin, bool high) {
+  set_pin(pin, high);
+  _delay_ms(DIGITAL_DELAY_MS);
+  set_pin(pin, ~high);
+  _delay_ms(DIGITAL_DELAY_MS);
 }
-*/
